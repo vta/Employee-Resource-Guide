0ca5d18e5da2583cf8927df10de9279b3cfa223f
Kyle Falconer <Kyle.Falconer@vta.org>
2016-11-04 11:59:48 -0700

    moved most of the rendering to be client-side now, added accordion support

--- make_guide.sh
+++ make_guide.sh
@@ -3,11 +3,11 @@
 
 # SOURCE_DIR is the folder which contains the PDF and markdown files to be
 # used in the creation of the resource guide.
-#SOURCE_DIR="./docs/Employee Resource Guide"
-SOURCE_DIR="./test_docs/Employee Resource Guide"
+SOURCE_DIR="./docs/Employee Resource Guide"
+#SOURCE_DIR="./test_docs/test_folder_2"
 
 # TARGET_DIR is the folder which will hold the resource guide once it is created
-TARGET_DIR="./Employee Resource Guide"
+TARGET_DIR="./out/Employee Resource Guide"
 
 # RESOURCES_DIR is the folder which contains files, like images, used in the final HTML
 RESOURCES_DIR="./resources"
@@ -15,8 +15,11 @@ RESOURCES_DIR="./resources"
 rm -rf "${TARGET_DIR}"
 
 if [ "$?" -eq "0" ]; then
-    python deguide.py "${SOURCE_DIR}" "${RESOURCES_DIR}"
+    python deguide.py "${SOURCE_DIR}" "${RESOURCES_DIR}" "${TARGET_DIR}"
 else 
 	echo Tried to delete the output folder but could not do so.
 	echo Please check that the files in "${TARGET_DIR}" are not in use and try again.
 fi
+
+# fixme
+# open ${TARGET_DIR}/index.html
